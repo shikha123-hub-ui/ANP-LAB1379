9bf7216807e0562ed11624fb222d9f459dedaeb4
shikha123-hub-ui <shikhavishwakarma235@gmail.com>
2025-07-23 19:08:59 +0530

    Add files via upload

---
+++ ReactFolder/Myfunction.js
@@ -0,0 +1,19 @@
+/*Functional Component Element
+import React from "react";
+function Demo()
+{
+    return(<h1>This is my first react application</h1>);
+}
+export default Demo;*/
+
+//class component elemennt
+import React from 'react';
+class Myclass extends React.Component
+{
+    render()
+    {
+        return(<h1>This is class component element</h1>);
+
+    }
+}
+export default Myclass;
