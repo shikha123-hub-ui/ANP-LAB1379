8c19f19c53e29035e3838132a68faa8ef37a8f91
shikha123-hub-ui <shikhavishwakarma235@gmail.com>
2025-07-23 19:08:26 +0530

    Add files via upload

---
+++ ReactFolder/App.js
@@ -0,0 +1,29 @@
+/* functional component
+import React from "react";
+import Demo from "./Myfunction";
+function App(){
+  return(
+    <div className="App">
+      <Demo/>
+      </div>
+    
+  );
+}
+export default App;*/
+
+//class component element
+import React from 'react';
+import Myclass from './Myfunction';
+function App()
+{
+  return(
+    <div className="App">
+      <Myclass/>
+      </div>
+  );
+}
+export default App;
+
+
+
+
